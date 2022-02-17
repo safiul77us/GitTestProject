1c1e57501993e9dc097c95d9d48844c0b07e8a4b
Safiul Alam <safiul77us@gmail.com>
2022-02-17 10:42:25 -0500

    3rd commit

--- gitHomeWork/src/gitHomeWork/gitHW.java
+++ gitHomeWork/src/gitHomeWork/gitHW.java
@@ -3,7 +3,11 @@ package gitHomeWork;
 public class gitHW {
 
 	public static void main(String[] args) {
-		System.out.println("Safiul Alam");
+		System.out.println("Safiul");
+		System.out.println("Alam ");
+		System.out.println(4);
+		
+		
 	}
 
 }
